f1ee8b8025144a9697a155f9a4057a6e3d406e51
James Macapagal <j.macapagal@gmail.com>
2022-05-18 08:21:58 -0500

    commit tree f690a0afcc4f422ef7ba025fa412daa92a742930

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = ["surprise"];
+
+
+function writeCards(names,eventName){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        debugger;
+    }
+    return messages;
+}
+
+console.log(writeCards(names,eventName))
+
+const int = 10
+function countDown(int){
+    let count = 0;
+    while (int >= 0){
+        console.log(int--)
+    }
+    return int
+}
+
+
+
